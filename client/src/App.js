4697dc1e01bf3676d0153e637de1a000a80c8c89
alontzukerman <alontzukerman@gmail.com>
2020-12-17 15:21:47 +0200

    apply logo

--- client/src/App.js
+++ client/src/App.js
@@ -9,23 +9,24 @@ import {
 } from "react-router-dom";
 import styled from "styled-components";
 import Navbar from "./components/Navbar";
-// import HomePage from "./pages/HomePage";
-// import SoldiersPage from "./pages/SoldiersPage";
-// import OneSoldierPage from "./pages/OneSoldierPage";
-// import NotesPage from "./pages/NotesPage";
-// import NohehutPage from "./pages/NohehutPage";
-// import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from './pages/NotFoundPage';
+import HomePage from "./pages/HomePage";
+import SoldiersPage from "./pages/SoldiersPage";
+import OneSoldierPage from "./pages/OneSoldierPage";
+import NotesPage from "./pages/NotesPage";
+import NohehutPage from "./pages/NohehutPage";
+import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
 import Cookies from "js-cookie";
 import { User } from "./context";
 import ErrorBoundary from "./components/ErrorBoundary";
-
-const HomePage = lazy(()=> import('./pages/HomePage'));
-const SoldiersPage = lazy(()=> import('./pages/SoldiersPage'));
-const OneSoldierPage = lazy(()=> import('./pages/OneSoldierPage'));
-const NotesPage = lazy(()=> import('./pages/NotesPage'));
-const NohehutPage = lazy(()=> import('./pages/NohehutPage'));
-const ProfilePage = lazy(()=> import('./pages/ProfilePage'));
+// const NotFoundPage = lazy(()=> import('./pages/NotFoundPage'))
+// const HomePage = lazy(()=> import('./pages/HomePage'));
+// const SoldiersPage = lazy(()=> import('./pages/SoldiersPage'));
+// const OneSoldierPage = lazy(()=> import('./pages/OneSoldierPage'));
+// const NotesPage = lazy(()=> import('./pages/NotesPage'));
+// const NohehutPage = lazy(()=> import('./pages/NohehutPage'));
+// const ProfilePage = lazy(()=> import('./pages/ProfilePage'));
 
 
 function App() {
@@ -96,6 +97,7 @@ function App() {
                 <ProfilePage />
               </Route>
               <Route path="*" exact>
+                {/* <NotFoundPage /> */}
                 <Redirect to="/" />
               </Route>
             </Switch>
